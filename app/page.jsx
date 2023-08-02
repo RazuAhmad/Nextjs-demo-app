0af3a5898ace59bddf7b00fe303af110ba55c0b5
razuAhmad <abdurrahmanrazu2000@gmail.com>
2023-08-02 17:05:17 +0600

    introduction done

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import '@styles/global.css';
 
 function Home() {
   return (
-    <section className='w-full flex-col'>
-        <h1>
+    <section className='w-full flex-col flex-center'>
+        <h1 className='head_text text-center'>
         Discover & Share
         <br className='max-md:hidden' />
-        <span>AI-Powered Prompts</span>
+        <span className='orange_gradient text-center'>AI-Powered Prompts</span>
         </h1>
+        <p className="desc text-center">
+
+        Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
+
+        </p>
     </section>
   )
 }
